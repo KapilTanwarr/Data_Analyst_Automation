bd38d93169c0429ece20a65bdf47cc322b61a453
Kapil Tanwar <kapiltanwarkt1011@gmail.com>
2025-06-25 03:20:57 +0530

    Delete data_ingestion.py

--- data_ingestion.py
+++
@@ -1,9 +0,0 @@
-
-import pandas as pd
-
-def load_data(path):
-    try:
-        df = pd.read_csv(path)
-        return df
-    except Exception as e:
-        raise Exception(f"Failed to load data: {e}")
